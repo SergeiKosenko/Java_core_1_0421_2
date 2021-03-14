f9afb5cc126850b046a63835cece14030f5ad6b7
Sergey-Kosenko <serg.cos@mail.ru>
2021-03-14 16:51:12 +0500

    gfsdfgsdfgsdfgsdf

--- src/main/java/ru/kosenko/lesson2/WhileFor.java
+++ src/main/java/ru/kosenko/lesson2/WhileFor.java
@@ -2,6 +2,8 @@ package ru.kosenko.lesson2;
 
 public class WhileFor {
     public static void main (String[] args){
+
+        System.out.println("Lesson2");
         System.out.println("Lesson2");
     }
 }
